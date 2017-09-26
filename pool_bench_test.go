4f76a6d6402e093b1a6e15e8ac3fd41612fca9f6
smallnest <smallnest@gmail.com>
2017-09-26 10:53:34 +0800

    add channel pool

--- pool_bench_test.go
+++ pool_bench_test.go
@@ -50,6 +50,20 @@ func BenchmarkPool_SyncPool(b *testing.B) {
 	})
 }
 
+func BenchmarkPool_ChanPool(b *testing.B) {
+	var p = NewChanPool(10000)
+	p.New = func() interface{} {
+		return 1
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			p.Put(1)
+			p.Get()
+		}
+	})
+}
+
 func BenchmarkPoolOverflow_Pool(b *testing.B) {
 	var p Pool
 	p.New = func() interface{} {
@@ -87,6 +101,24 @@ func BenchmarkPoolOverflow_ShardPool(b *testing.B) {
 
 func BenchmarkPoolOverflow_SyncPool(b *testing.B) {
 	var p sync.Pool
+	p.New = func() interface{} {
+		return 1
+	}
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for b := 0; b < 1000; b++ {
+				p.Put(1)
+			}
+			for b := 0; b < 1000; b++ {
+				p.Get()
+			}
+		}
+	})
+}
+
+func BenchmarkPoolOverflow_ChanPool(b *testing.B) {
+	var p = NewChanPool(10000)
 	p.New = func() interface{} {
 		return 1
 	}
